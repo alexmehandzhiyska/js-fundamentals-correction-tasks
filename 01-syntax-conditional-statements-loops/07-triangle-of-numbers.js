cf81fa203febffaf5c1cc4c472f7c7c83948dedf
Alexandrina Mehandzhiyska <73063309+alexmehandzhiyska@users.noreply.github.com>
2024-05-16 17:46:22 +0300

    Added solution for correction to 07. Triangle of numbers

---
+++ 01-syntax-conditional-statements-loops/07-triangle-of-numbers.js
@@ -0,0 +1,7 @@
+function triangleOfNumbers(n) {
+    for (let curNum = 1; curNum < n; curNum++) {
+        for (let numCount = 1; numCount < n; numCount++) {
+            console.log(curNum);
+        }
+    }
+}
